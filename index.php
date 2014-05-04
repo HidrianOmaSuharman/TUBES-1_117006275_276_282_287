092b7f8e0259d10ca12cf7540f6fedb8d14ddecd
tejapermana <teja.permana@student.unsil.ac.id>
2014-05-04 21:32:42 +0700

    Create Liputan6.php, liputan6impl.php, index.php

---
+++ index.php
@@ -0,0 +1,7 @@
+<HTML>
+<ul>
+		<li><a href="VivanewsImpl.php">VIVA NEWS</a></li> 
+		<li><a href="Liputan6Impl.php">Liputan 6</a></li>
+		<li><a href="BeritaTeknologiImpl.php">Berita Teknologi</a></li> 
+</ul>
+</HTML>
